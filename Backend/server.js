e331c1d9b3f18ad89289659677f819a5cf6c6181
Ishan <ishanprashar97085@gmail.com>
2025-12-03 22:20:54 +0530

    k

--- Backend/server.js
+++ Backend/server.js
@@ -15,16 +15,16 @@ import cors from 'cors'
 
 connectDatabase()
 dotenv.config()
-const corsOptions = {
-  origin: '*', // Allow only this origin
-  methods: ['GET', 'POST','PUT','PATCH','DELETE'], // Allow only GET and POST requests
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
-  credentials: true // Allow sending cookies/credentials
-};
+// const corsOptions = {
+//   origin: '*', // Allow only this origin
+//   methods: ['GET', 'POST','PUT','PATCH','DELETE'], // Allow only GET and POST requests
+//   allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+//   credentials: true // Allow sending cookies/credentials
+// };
 const app=express()
 const port=process.env.PORT
 
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(express.json())
 app.use('/api/auth',AuthRoutes)
 app.use('/api/tasks',TaskRoutes)
